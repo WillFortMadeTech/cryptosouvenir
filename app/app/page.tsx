5add540d6d05096cb2785bdb8b0ae280f17abfa8
Will Fort <will.fort@madetech.com>
2026-05-23 22:18:16 +0100

    cube test

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,9 +1,30 @@
-import Image from "next/image";
+'use client'
+
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    Testing
-   </div>
-  );
+	const ref = useRef<HTMLDivElement>(null)
+
+	useEffect(() => {
+		const scene = new THREE.Scene();
+		const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+		const renderer = new THREE.WebGLRenderer();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		ref.current!.appendChild(renderer.domElement);
+
+		const cube = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshBasicMaterial({ color: 0x00ff00 }));
+		scene.add(cube);
+		camera.position.z = 5;
+
+		const animate = () => {
+			requestAnimationFrame(animate);
+			cube.rotation.x += 0.01;
+			renderer.render(scene, camera);
+
+		}
+		animate()
+
+	}, []);
+	return <div ref={ref} />
 }
